e0307d414ae152e41ae9167e730a817ea43b5e7e
litianhua <1040240425@qq.com>
2020-09-01 14:51:50 +0800

    结构调整

--- base-code/src/test/java/com/doudanhua/basecode/BaseCodeApplicationTests.java
+++ base-code/src/test/java/com/doudanhua/basecode/BaseCodeApplicationTests.java
@@ -1,6 +1,5 @@
 package com.doudanhua.basecode;
 
-import com.doudanhua.basecode.sl4jDemo.AopMethodTest;
 import com.doudanhua.basecode.dto.DogDto;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -14,15 +13,6 @@ class BaseCodeApplicationTests {
     @Test
     void contextLoads() {
     }
-
-    @Autowired
-    AopMethodTest aopMethodTest;
-
-    @Test
-    public void aopMethod_test(){
-        aopMethodTest.say("555");
-    }
-
     @Test
     public void sl4j_json_test(){
         DogDto dogDto=new DogDto();
